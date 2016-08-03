34870041082e3fd752204c182c69a69e3e7a6a97
popomore <sakura9515@gmail.com>
2016-08-03 16:30:16 +0800

    refactor: change loadDirs to getLoadUnits
    loadUnit is a directory that can be loaded by EggLoader

--- test/egg_loader.test.js
+++ test/egg_loader.test.js
@@ -2,26 +2,9 @@
 
 require('should');
 const mm = require('mm');
-const utils = require('./utils');
 
 describe('test/egg_loader.test.js', function() {
 
   afterEach(mm.restore);
-
-
-  describe('loadDirs', function() {
-
-    it('should get plugin dir', function() {
-      const app = utils.createApp('plugin');
-      const dirs = app.loader.loadDirs();
-      dirs.length.should.eql(10);
-    });
-
-    it('should not get plugin dir', function() {
-      const loader = new utils.Loader('plugin');
-      const dirs = loader.loadDirs();
-      dirs.length.should.eql(2);
-    });
-  });
 
 });
