0a169eaec13bbc7d0776e1739cd5b3e803a4eb53
Haoliang Gao <sakura9515@gmail.com>
2016-08-19 16:05:03 +0800

    test: delete type testcase (#15)

--- test/fixtures/dont-load-plugin/config/plugin.js
+++ test/fixtures/dont-load-plugin/config/plugin.js
@@ -3,12 +3,10 @@ const path = require('path');
 exports.testMe = {
   enable: true,
   env: ['local'],
-  type: ['alipay'],
   path: path.join(__dirname, '../../../plugins/test-me')
 };
 
 exports.testMeAli = {
   enable: true,
-  type: ['ali'],
   path: path.join(__dirname, '../../../plugins/test-me-ali')
 };
