47dff2857f79372c02dc09834fb777abaf306b53
Hengfei Zhuang <zhuanghengfei@gmail.com>
2016-10-13 13:38:45 +0800

    fix: always get the executor's homedir (#17)

--- test/utils/index.test.js
+++ test/utils/index.test.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const mm = require('mm');
+const os = require('os');
 const utils = require('../../lib/utils');
+const should = require('should');
 
 describe('test/utils/index.test.js', () => {
 
@@ -9,12 +11,21 @@ describe('test/utils/index.test.js', () => {
 
   describe('utils.getHomedir()', () => {
     it('should return process.env.HOME', () => {
+      if (os.userInfo && os.userInfo().homedir) {
+        const userInfo = os.userInfo();
+        delete userInfo.homedir;
+        mm(os, 'userInfo', () => userInfo);
+      }
       utils.getHomedir().should.equal(process.env.HOME);
     });
 
     it('should return /home/admin when process.env.HOME is not exist', () => {
       mm(process.env, 'HOME', '');
-      utils.getHomedir().should.equal('/home/admin');
+      if (os.userInfo && os.userInfo().homedir) {
+        should.ok(utils.getHomedir().indexOf(process.env.USER) > -1);
+      } else {
+        utils.getHomedir().should.equal('/home/admin');
+      }
     });
   });
 });
