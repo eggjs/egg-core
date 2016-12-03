20fef6f1fce2c1b56d9e867ee686860f4a7a596d
wanghx <whxaxes@gmail.com>
2016-12-03 10:14:13 +0800

    fix: router.url can't parse multi params right

--- test/fixtures/router-app/app/router.js
+++ test/fixtures/router-app/app/router.js
@@ -5,5 +5,6 @@ module.exports = function (app) {
   app.resources('members', '/members', app.controller.members);
   app.resources('/comments', app.controller.comments);
   app.get('comment_index', '/comments/:id?filter=', app.controller.comments.index);
+  app.get('params', '/params/:a/:b', app.controller.locals.router);
   app.register('/comments', [ 'post' ] , app.controller.comments.new);
 };
