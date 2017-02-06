c68d9fffaa5654a8f8a8ba78840e4c92e81f2e4a
popomore <sakura9515@gmail.com>
2017-02-06 16:14:44 +0800

    feat: app.beforeStart support async function same as beforeClose

--- test/fixtures/beforestart-params-error/app.js
+++ test/fixtures/beforestart-params-error/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(app) {
-  app.beforeStart(() => {});
+  app.beforeStart();
 };
--- test/fixtures/beforestart/app.js
+++ test/fixtures/beforestart/app.js
@@ -1,9 +1,28 @@
 'use strict';
-
-module.exports = function(app) {
-  app.beforeStart(function*() {
-    if (!app.isReady) throw new Error('not ready');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+    function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+    function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+    step((generator = generator.apply(thisArg, _arguments)).next());
   });
-
-  app.isReady = true;
+};
+const sleep = require('ko-sleep');
+module.exports = function (app) {
+  app.beforeStart(function() {
+    app.beforeStartFunction = true;
+  });
+  app.beforeStart(function* () {
+    yield sleep(1000);
+    app.beforeStartGeneratorFunction = true;
+  });
+  app.beforeStart(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+      yield sleep(1000);
+      app.beforeStartAsyncFunction = true;
+    });
+  });
+  app.beforeStartFunction = false;
+  app.beforeStartAsyncFunction = false;
+  app.beforeStartGeneratorFunction = false;
 };
