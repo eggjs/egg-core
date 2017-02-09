6ef3ab5f94b5faab64cf8086a2544cb4ef089225
popomore <sakura9515@gmail.com>
2017-02-09 20:27:58 +0800

    fix: make sure close once

--- test/egg.test.js
+++ test/egg.test.js
@@ -4,6 +4,7 @@ const mm = require('mm');
 const util = require('util');
 const path = require('path');
 const assert = require('assert');
+const spy = require('spy');
 const utils = require('./utils');
 const EggCore = require('..').EggCore;
 const EggLoader = require('..').EggLoader;
@@ -223,6 +224,19 @@ describe('test/egg.test.js', () => {
         assert(err.message === 'removeAllListeners error');
         done();
       });
+    });
+
+    it('should close only once', done => {
+      const fn = spy();
+      app = utils.createApp('close');
+      app.beforeClose(fn);
+      Promise.all([
+        app.close(),
+        app.close(),
+      ]).then(() => {
+        assert(fn.callCount === 1);
+        done();
+      }).catch(done);
     });
   });
 
