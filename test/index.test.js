09cb2dcb509ac3760f8c5297ad87bf75fbe55172
Yiyu He <dead-horse@users.noreply.github.com>
2017-04-11 14:08:22 +0800

    feat: expose BaseContextClass (#71)

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('assert');
+const EggCore = require('..');
+
+describe('test/index.test.js', () => {
+  it('should expose properties', () => {
+    assert(EggCore.EggCore);
+    assert(EggCore.EggLoader);
+    assert(EggCore.BaseContextClass);
+  });
+});
