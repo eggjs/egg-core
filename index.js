1fd747a8930dfcd0aa6ca597d4e756c08c378f66
dead-horse <dead_horse@qq.com>
2017-04-11 12:05:01 +0800

    feat: expose BaseContextClass

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports.EggCore = require('./lib/egg');
-module.exports.EggLoader = require('./lib/loader/egg_loader');
+const EggCore = require('./lib/egg');
+const EggLoader = require('./lib/loader/egg_loader');
+const BaseContextClass = require('./lib/utils/base_context_class');
+
+module.exports = {
+  EggCore,
+  EggLoader,
+  BaseContextClass,
+};
