7f7a845899a04d11e76179c9749555c5ba4592b4
popomore <sakura9515@gmail.com>
2017-04-17 23:18:13 +0800

    f

--- test/utils/index.test.js
+++ test/utils/index.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mm = require('mm');
-const os = require('os');
 const path = require('path');
 const assert = require('assert');
 const sleep = require('mz-modules/sleep');
@@ -10,29 +9,6 @@ const utils = require('../../lib/utils');
 describe('test/utils/index.test.js', () => {
 
   afterEach(mm.restore);
-
-  describe('utils.getHomedir()', () => {
-    it('should return process.env.HOME', () => {
-      if (os.userInfo && os.userInfo().homedir) {
-        const userInfo = os.userInfo();
-        delete userInfo.homedir;
-        mm(os, 'userInfo', () => userInfo);
-      }
-      assert(utils.getHomedir() === process.env.HOME);
-    });
-
-    it('should return /home/admin when process.env.HOME is not exist', () => {
-      mm(process.env, 'HOME', '');
-      mm(os, 'userInfo', null);
-      mm(os, 'homedir', null);
-      assert(utils.getHomedir() === '/home/admin');
-    });
-
-    it('should return when EGG_HOME exists', () => {
-      mm(process.env, 'EGG_HOME', '/path/to/home');
-      assert(utils.getHomedir() === '/path/to/home');
-    });
-  });
 
   describe('callFn', () => {
 
