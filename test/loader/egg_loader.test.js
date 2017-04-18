e1ba34707d0ce966454097e5fb6278df2462f7a2
Haoliang Gao <sakura9515@gmail.com>
2017-04-18 12:36:38 +0800

    refactor: export getHomedir that can be extended (#78)

--- test/loader/egg_loader.test.js
+++ test/loader/egg_loader.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const assert = require('assert');
+const os = require('os');
+const mm = require('mm');
 const utils = require('../utils');
 
 describe('test/loader/egg_loader.test.js', () => {
@@ -13,6 +15,31 @@ describe('test/loader/egg_loader.test.js', () => {
   it('should container FileLoader and ContextLoader', () => {
     assert(app.loader.FileLoader);
     assert(app.loader.ContextLoader);
+  });
+
+  describe('loader.getHomedir()', () => {
+    afterEach(mm.restore);
+
+    it('should return process.env.HOME', () => {
+      if (os.userInfo && os.userInfo().homedir) {
+        const userInfo = os.userInfo();
+        delete userInfo.homedir;
+        mm(os, 'userInfo', () => userInfo);
+      }
+      assert(app.loader.getHomedir() === process.env.HOME);
+    });
+
+    it('should return /home/admin when process.env.HOME is not exist', () => {
+      mm(process.env, 'HOME', '');
+      mm(os, 'userInfo', null);
+      mm(os, 'homedir', null);
+      assert(app.loader.getHomedir() === '/home/admin');
+    });
+
+    it('should return when EGG_HOME exists', () => {
+      mm(process.env, 'EGG_HOME', '/path/to/home');
+      assert(app.loader.getHomedir() === '/path/to/home');
+    });
   });
 
 });
