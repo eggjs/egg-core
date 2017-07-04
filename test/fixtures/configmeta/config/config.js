5c45eb3e57ae462e28984ab46cf5a08f43250db6
fengmk2 <fengmk2@gmail.com>
2017-07-05 01:51:17 +0800

    fix: should ignore Object.getPrototypeOf check on null/undefined (#107)
    bugfix for https://github.com/eggjs/egg-core/pull/106

--- test/fixtures/configmeta/config/config.js
+++ test/fixtures/configmeta/config/config.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const urllib = require('urllib');
+const HttpClient2 = require('urllib').HttpClient2;
+const urllib = new HttpClient2();
 
 exports.urllib = {
-  keepAlive: false
+  keepAlive: false,
+  foo: null,
+  bar: undefined,
+  n: 1,
+  dd: new Date(),
+  httpclient: urllib,
 };
 
 exports.buffer = new Buffer('1234');
 exports.array = [];
 
 exports.console = console;
+
+exports.zero = 0;
+exports.number = 1;
+exports.ok = true;
+exports.f = false;
+exports.no = null;
+exports.empty = {};
+exports.date = new Date();
+exports.ooooo = urllib;
