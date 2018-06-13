39c319b711465434d9ea4c64880bcc13769ea8d2
waiting <1661926154@qq.com>
2018-06-13 18:27:29 +0800

    chore(typings): add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+import * as KoaApplication from 'koa';
+
+
+export interface EggCoreOptions {
+  baseDir: string;
+  type: 'application' | 'agent';
+  typescript?: boolean;
+  plugins?: any;
+}
+
+export interface EggLoaderOptions {
+  app: EggCore;
+  baseDir: string;
+  logger: Logger;
+  plugins?: any;
+}
+
+export interface Logger {
+  info(msg: any, ...args: any[]): void;
+  warn(msg: any, ...args: any[]): void;
+  debug(msg: any, ...args: any[]): void;
+  error(msg: any, ...args: any[]): void;
+}
+
+export class EggCore extends KoaApplication {
+  constructor(options: EggCoreOptions);
+}
+
+export class EggLoader {
+  constructor(options: EggLoader);
+}
