6d910f61701936c075d84c58bfcd8926da85fe5b
TZ | 天猪 <atian25@qq.com>
2018-07-08 00:20:39 +0800

    feat: loadUnit should exports name (#172)

--- test/loader/get_load_units.test.js
+++ test/loader/get_load_units.test.js
@@ -16,10 +16,14 @@ describe('test/get_load_units.test.js', function() {
     delete app.loader.dirs;
     const units = app.loader.getLoadUnits();
     assert(units.length === 12);
+    assert(units[2].type === 'plugin');
+    assert(units[2].name === 'package');
     assert(units[10].type === 'framework');
     assert(units[10].path === utils.getFilepath('egg'));
+    assert(units[10].name === 'egg');
     assert(units[11].type === 'app');
     assert(units[11].path === utils.getFilepath('plugin'));
+    assert(units[11].name === 'plugin');
   });
 
   it('should not get plugin dir', function() {
