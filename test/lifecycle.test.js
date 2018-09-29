58a49e46684bf6adceada18abb1fe1b7086a764e
initialwu <initialwu@tencent.com>
2018-09-29 11:33:02 +0800

    fix(lifecycle): forbid adding hook atfter initialization

---
+++ test/lifecycle.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const assert = require('assert');
+
+const Lifecycle = require('../lib/lifecycle.js');
+const EggCore = require('..').EggCore;
+describe('test/lifecycle.js', () => {
+  it('should forbid adding hook atfter initialization', () => {
+    const lifecycle = new Lifecycle({
+      baseDir: '.',
+      app: new EggCore(),
+    });
+
+    lifecycle.init();
+    assert.throws(() => {
+      lifecycle.addBootHook(
+        class Hook {
+          constructor(app) {
+            this.app = app;
+          }
+          configDidLoad() {
+            console.log('test');
+          }
+        }
+      );
+    }, /do not add hook when lifecycle has been initialized/);
+
+    assert.throws(() => {
+      lifecycle.addBootHook(() => {
+        console.log('test');
+      });
+    }, /do not add hook when lifecycle has been initialized/);
+  });
+});
