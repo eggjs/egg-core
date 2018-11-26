9b41bf7c4529e0386d436a6180eb4f6a1282ffc6
wanghx <whx89768@alibaba-inc.com>
2018-11-26 10:37:37 +0800

    f

--- test/jest.test.js
+++ test/jest.test.js
@@ -7,20 +7,38 @@ const utils = require('../lib/utils');
 const { fork } = require('child_process');
 
 describe('test/jest.test.js', () => {
-  afterEach(mm.restore);
+  let extensions;
 
-  it('should has default extensions if require.extension is empty', () => {
-    const extensions = {};
+  beforeEach(() => {
+    extensions = {};
     Object.keys(require.extensions).forEach(ext => {
       extensions[ext] = require.extensions[ext];
+    });
+    delete utils._extensions;
+  });
+
+  afterEach(() => {
+    Object.keys(extensions).forEach(ext => {
+      require.extensions[ext] = extensions[ext];
+    });
+
+    mm.restore();
+  });
+
+  it('should has default extensions if require.extension is empty', () => {
+    Object.keys(require.extensions).forEach(ext => {
+      delete require.extensions[ext];
+    });
+    assert(utils.extensions.length > 0);
+  });
+
+  it('should has ts extensions if require.extension is empty and env.EGG_TYPESCRIPT is true', () => {
+    Object.keys(require.extensions).forEach(ext => {
       delete require.extensions[ext];
     });
     mm(process.env, 'EGG_TYPESCRIPT', 'true');
     assert(utils.extensions.length > 0);
     assert(utils.extensions.includes('.ts'));
-    Object.keys(extensions).forEach(ext => {
-      require.extensions[ext] = extensions[ext];
-    });
   });
 
   it('should works without error with jest', done => {
