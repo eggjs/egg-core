b207f89b214cfd2a432df87d771a5126ffce9289
killa <killa123@126.com>
2020-09-08 13:39:16 +0800

    feat: timing support Process Start/Script Start (#222)
    * feat: timing support Process Start/Script Start
    * doc: add timing doc
    
    Co-authored-by: TZ | 天猪 <atian25@qq.com>

--- test/utils/timing.test.js
+++ test/utils/timing.test.js
@@ -13,14 +13,14 @@ describe('test/utils/timing.test.js', () => {
     timing.end('b');
 
     const json = timing.toJSON();
-    assert(json.length === 2);
+    assert(json.length === 3);
 
-    assert(json[0].name === 'a');
-    assert(json[0].end - json[0].start === json[0].duration);
-    assert(json[0].pid === process.pid);
-    assert(json[1].name === 'b');
+    assert(json[1].name === 'a');
     assert(json[1].end - json[1].start === json[1].duration);
     assert(json[1].pid === process.pid);
+    assert(json[2].name === 'b');
+    assert(json[2].end - json[2].start === json[2].duration);
+    assert(json[2].pid === process.pid);
   });
 
   it('should set item when start', () => {
@@ -28,10 +28,10 @@ describe('test/utils/timing.test.js', () => {
     timing.start('a');
 
     const json = timing.toJSON();
-    assert(json[0].name === 'a');
-    assert(json[0].start);
-    assert(json[0].end === undefined);
-    assert(json[0].duration === undefined);
+    assert(json[1].name === 'a');
+    assert(json[1].start);
+    assert(json[1].end === undefined);
+    assert(json[1].duration === undefined);
   });
 
   it('should ignore start when name is empty', () => {
@@ -39,22 +39,22 @@ describe('test/utils/timing.test.js', () => {
     timing.start();
 
     const json = timing.toJSON();
-    assert(json.length === 0);
+    assert(json.length === 1);
   });
 
   it('should throw when name exists', () => {
     const timing = new Timing();
     timing.start('a');
-    assert(timing.toJSON().length === 1);
+    assert(timing.toJSON().length === 2);
 
     timing.start('a');
-    assert(timing.toJSON().length === 2);
+    assert(timing.toJSON().length === 3);
   });
 
   it('should ignore end when name dont exist', () => {
     const timing = new Timing();
     timing.end();
-    assert(timing.toJSON().length === 0);
+    assert(timing.toJSON().length === 1);
   });
 
   it('should enable/disable', () => {
@@ -74,9 +74,9 @@ describe('test/utils/timing.test.js', () => {
 
     const json = timing.toJSON();
 
-    assert(json[0].name === 'a');
-    assert(json[1].name === 'c');
-    assert(json.length === 2);
+    assert(json[1].name === 'a');
+    assert(json[2].name === 'c');
+    assert(json.length === 3);
   });
 
   it('should clear', () => {
@@ -85,7 +85,7 @@ describe('test/utils/timing.test.js', () => {
     timing.end('a');
 
     const json = timing.toJSON();
-    assert(json[0].name === 'a');
+    assert(json[1].name === 'a');
 
     timing.clear();
 
@@ -104,5 +104,13 @@ describe('test/utils/timing.test.js', () => {
     assert.throws(() => {
       timing.end('a');
     }, /should run timing.start\('a'\) first/);
+  });
+
+  it('should init process start time', () => {
+    const timing = new Timing();
+
+    const processStart = timing.toJSON().find(item => item.name === 'Process Start');
+    assert(processStart);
+    assert(processStart.start);
   });
 });
