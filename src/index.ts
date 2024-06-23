da6c193100d900c7a43e65b4308a3f31399968b9
fengmk2 <suqian.yf@antgroup.com>
2024-06-23 21:41:39 +0800

    fix: should export everything

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import utils from './utils/index.js';
 
-export * from './egg.js';
-export * from './loader/egg_loader.js';
-export * from './base_context_class.js';
 export { utils };
+
+export * from './egg.js';
+export * from './base_context_class.js';
+export * from './lifecycle.js';
+export * from './loader/egg_loader.js';
+export * from './loader/file_loader.js';
+export * from './loader/context_loader.js';
+export * from './utils/sequencify.js';
+export * from './utils/timing.js';
